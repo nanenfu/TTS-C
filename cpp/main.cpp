3b3483803fb9b797df74ef5f69d6bf34ecf90081
Dzmitry Plashchynski <plashchynski@gmail.com>
2024-10-22 02:20:08 +0300

    ONNX files

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -22,12 +22,12 @@ void save_audio(const std::vector<float>& audio_data, const std::string& file_pa
 }
 
 // Adapted from the Enfu's code
-int main() {
+void run_vits_onnx_model(std::vector<int64_t> text_seq, std::vector<int64_t> pred_semantic) {
     Ort::Env env(ORT_LOGGING_LEVEL_WARNING, "test");
     Ort::SessionOptions session_options;
     session_options.SetIntraOpNumThreads(1);
 
-    std::string onnx_model_path = "/Users/dima/dev/Text-to-speech-new/tts_new/onnx/test3/test3_vits.onnx";
+    std::string onnx_model_path = "onnx/test3/test3_vits.onnx";
     Ort::Session session(env, onnx_model_path.c_str(), session_options);
 
     auto memory_info = Ort::MemoryInfo::CreateCpu(OrtArenaAllocator, OrtMemTypeDefault);
@@ -46,18 +46,13 @@ int main() {
     std::cout << "Input 1 name: " << input_name2 << std::endl;
     std::cout << "Output name: " << output_name << std::endl;
 
-    std::vector<int64_t> text_seq = {60, 13, 75, 80, 27, 12, 80, 88, 13, 90, 13, 75, 1, 91, 58, 61,
-                                        10, 64, 63, 42, 61, 55, 80, 55, 49, 91, 58, 80, 74, 22, 3};
+
     std::vector<int64_t> text_seq_modifiable = text_seq;
     std::vector<int64_t> input_dims1 = {1, static_cast<int64_t>(text_seq_modifiable.size())};
 
     Ort::Value input_tensor1 = Ort::Value::CreateTensor<int64_t>(
         memory_info, text_seq_modifiable.data(), text_seq_modifiable.size(), input_dims1.data(), input_dims1.size());
 
-    std::vector<int64_t> pred_semantic = {361, 643, 479, 150, 753, 760, 644, 45, 996, 42, 744, 785, 790, 619, 251, 319, 488,
-                                        783, 127, 660, 331, 637, 32, 528, 934, 303, 740, 9, 908, 837, 325, 201, 55, 385,
-                                        921, 256, 369, 887, 827, 355, 145, 293, 989, 988, 84, 378, 727, 180, 180, 263, 496,
-                                        175, 902, 278, 748, 748, 52, 0};
     std::vector<int64_t> pred_semantic_modifiable = pred_semantic;
     std::vector<int64_t> input_dims2 = {1, 1, static_cast<int64_t>(pred_semantic_modifiable.size())};
 
@@ -74,7 +69,20 @@ int main() {
     size_t audio_output_size = outputs[0].GetTensorTypeAndShapeInfo().GetElementCount();
 
     std::vector<float> output = std::vector<float>(audio_output, audio_output + audio_output_size);
-    save_audio(output, "ntout.wav", 32000);
-    std::cout << "Audio saved as 'ntout.wav'" << std::endl;
+    save_audio(output, "output.wav", 32000);
+    std::cout << "Audio saved as 'output.wav'" << std::endl;
+}
+
+
+int main() {
+    std::vector<int64_t> text_seq = {60, 13, 75, 80, 27, 12, 80, 88, 13, 90, 13, 75, 1, 91, 58, 61,
+                                        10, 64, 63, 42, 61, 55, 80, 55, 49, 91, 58, 80, 74, 22, 3};
+
+    std::vector<int64_t> pred_semantic = {361, 643, 479, 150, 753, 760, 644, 45, 996, 42, 744, 785, 790, 619, 251, 319, 488,
+                                        783, 127, 660, 331, 637, 32, 528, 934, 303, 740, 9, 908, 837, 325, 201, 55, 385,
+                                        921, 256, 369, 887, 827, 355, 145, 293, 989, 988, 84, 378, 727, 180, 180, 263, 496,
+                                        175, 902, 278, 748, 748, 52, 0};
+
+    run_vits_onnx_model(text_seq, pred_semantic);
     return 0;
 }
