d1a889abf73f15a4898a39911dbcf813d4ee6adb
Dzmitry Plashchynski <plashchynski@gmail.com>
2024-11-19 22:01:34 +0300

    Fix Swift package dependencies

--- Package.swift
+++ Package.swift
@@ -11,13 +11,18 @@ let package = Package(
             name: "SealTTS",
             targets: ["SealTTS"]),
     ],
+    dependencies: [
+        .package(url: "https://github.com/microsoft/onnxruntime-swift-package-manager.git", .exact("1.19.2"))
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "SealTTS",
+            dependencies: ["onnxruntime-swift-package-manager"],
             path: "cpp",
-            exclude: ["main.cpp"],
+            sources: ["encoder.cpp", "fsdecoder.cpp", "nlp.cpp", "onnx_utils.cpp", "ssdecoder.cpp", "ssl_content.cpp",
+                        "string_utils.cpp", "text_preprocessor.cpp", "tts_engine.cpp", "vits.cpp"],
             publicHeadersPath: "."
         )
     ]
